ee49d1c36bdb979887b009c74b720939d1e23938
ekhel <blackmanskill@gmail.com>
2019-11-16 02:05:26 +0900

    ChangeApi Method

--- app/pasarikan/api/urls.py
+++ app/pasarikan/api/urls.py
@@ -1,31 +1,40 @@
-from django.urls import path, include
+from ..api.views import ProdukViewSet
+from rest_framework.routers import DefaultRouter
 
-from .views import (
-    NelayanListView, 
-    NelayanDetailView,
-    NelayanCreateView,
-    NelayanUpdateView,
-    NelayanDeleteView, 
-    ProdukListView, 
-    ProdukDetailView, 
-    DistrikListView, 
-    DistrikDetailView, 
-    KampungListView, 
-    KampungDetailView,
-    OrderListView,
-)
+router = DefaultRouter()
+router.register(r'produk', ProdukViewSet, basename='produk')
+urlpatterns = router.urls
 
-urlpatterns = [
-    path('nelayan/', NelayanListView.as_view()),
-    path('nelayan/create', NelayanCreateView.as_view()),
-    path('nelayan/<pk>/update/', NelayanUpdateView.as_view()),
-    path('nelayan/<pk>/delete/', NelayanDeleteView.as_view()),
-    path('nelayan/<pk>', NelayanDetailView.as_view()),
-    path('produk/', ProdukListView.as_view()),
-    path('produk/<pk>', ProdukDetailView.as_view()),
-    path('distrik/', DistrikListView.as_view()),
-    path('distrik/<pk>', DistrikDetailView.as_view()),
-    path('kampung/', KampungListView.as_view()),
-    path('kampung/<pk>', KampungDetailView.as_view()),
-    path('orderitem/', OrderListView.as_view()),
-]
+# from django.urls import path, include
+
+# from .views import (
+#     NelayanListView, 
+#     NelayanDetailView,
+#     NelayanCreateView,
+#     NelayanUpdateView,
+#     NelayanDeleteView, 
+#     ProdukListView, 
+#     ProdukDetailView, 
+#     DistrikListView, 
+#     DistrikDetailView, 
+#     KampungListView, 
+#     KampungDetailView,
+#     OrderListView,
+# )
+
+# urlpatterns = [
+#     path('nelayan/', NelayanListView.as_view()),
+#     path('nelayan/create', NelayanCreateView.as_view()),
+#     path('nelayan/<pk>/update/', NelayanUpdateView.as_view()),
+#     path('nelayan/<pk>/delete/', NelayanDeleteView.as_view()),
+#     path('nelayan/<pk>', NelayanDetailView.as_view()),
+#     path('produk/', ProdukListView.as_view()),
+#     path('produk/<pk>', ProdukDetailView.as_view()),
+#     path('distrik/', DistrikListView.as_view()),
+#     path('distrik/<pk>', DistrikDetailView.as_view()),
+#     path('kampung/', KampungListView.as_view()),
+#     path('kampung/<pk>', KampungDetailView.as_view()),
+#     path('orderitem/', OrderListView.as_view()),
+# ]
+
+
--- app/pasarikan/api/views.py
+++ app/pasarikan/api/views.py
@@ -1,78 +1,84 @@
-from rest_framework.generics import ListAPIView, RetrieveAPIView, CreateAPIView, UpdateAPIView, DestroyAPIView
+# from rest_framework.generics import ListAPIView, RetrieveAPIView, CreateAPIView, UpdateAPIView, DestroyAPIView
 from ..models import Nelayan, Produk, Distrik, Kampung, OrderItem, Order
+from rest_framework import viewsets
+
 from .serializers import (
-    NelayanSerializer, 
-    ProdukSerializer, 
-    DistrikSerializer, 
-    KampungSerializer,
-    OrderItemSerializer,
-)
+     NelayanSerializer, 
+     ProdukSerializer, 
+     DistrikSerializer, 
+     KampungSerializer,
+     OrderItemSerializer,
+ )
+
+class ProdukViewSet(viewsets.ModelViewSet):
+    serializer_class = ProdukSerializer
+    queryset = Produk.objects.all()
 
 # Start API Nelayan GET, POST, UPDATE, DELETE, RETRIVE
 
-class NelayanListView(ListAPIView):
-    queryset = Nelayan.objects.all()
-    serializer_class = NelayanSerializer
+# class NelayanListView(ListAPIView):
+#     queryset = Nelayan.objects.all()
+#     serializer_class = NelayanSerializer
 
-class NelayanDetailView(RetrieveAPIView):
-    queryset = Nelayan.objects.all()
-    serializer_class = NelayanSerializer
+# class NelayanDetailView(RetrieveAPIView):
+#     queryset = Nelayan.objects.all()
+#     serializer_class = NelayanSerializer
 
-class NelayanCreateView(CreateAPIView):
-    queryset = Nelayan.objects.all()
-    serializer_class = NelayanSerializer
+# class NelayanCreateView(CreateAPIView):
+#     queryset = Nelayan.objects.all()
+#     serializer_class = NelayanSerializer
 
-class NelayanUpdateView(UpdateAPIView):
-    queryset = Nelayan.objects.all()
-    serializer_class = NelayanSerializer
+# class NelayanUpdateView(UpdateAPIView):
+#     queryset = Nelayan.objects.all()
+#     serializer_class = NelayanSerializer
 
-class NelayanDeleteView(DestroyAPIView):
-    queryset = Nelayan.objects.all()
-    serializer_class = NelayanSerializer
+# class NelayanDeleteView(DestroyAPIView):
+#     queryset = Nelayan.objects.all()
+#     serializer_class = NelayanSerializer
 
 # END API Nelanyan
 
 # Start API Produk GET, POST,
 
-class ProdukListView(ListAPIView):
-    queryset = Produk.objects.all()
-    serializer_class = ProdukSerializer
+# class ProdukListView(ListAPIView):
+#     queryset = Produk.objects.all()
+#     serializer_class = ProdukSerializer
 
-class ProdukDetailView(RetrieveAPIView):
-    queryset = Produk.objects.all()
-    serializer_class = ProdukSerializer
+# class ProdukDetailView(RetrieveAPIView):
+#     queryset = Produk.objects.all()
+#     serializer_class = ProdukSerializer
 
-class ProdukCreateView(CreateAPIView):
-    queryset = Produk.objects.all()
-    serializer_class = ProdukSerializer
+# class ProdukCreateView(CreateAPIView):
+#     queryset = Produk.objects.all()
+#     serializer_class = ProdukSerializer
 
 # END API Produk
 
 # Start API Distrik GET, RETRIVE
 
-class DistrikListView(ListAPIView):
-    queryset = Distrik.objects.all()
-    serializer_class = DistrikSerializer
+# class DistrikListView(ListAPIView):
+#     queryset = Distrik.objects.all()
+#     serializer_class = DistrikSerializer
 
-class DistrikDetailView(RetrieveAPIView):
-    queryset = Distrik.objects.all()
-    serializer_class = DistrikSerializer
+# class DistrikDetailView(RetrieveAPIView):
+#     queryset = Distrik.objects.all()
+#     serializer_class = DistrikSerializer
 
 # END API Distrik
 
 # Start API Kampung GET, RETRIVE
-class KampungListView(ListAPIView):
-    queryset = Kampung.objects.all()
-    serializer_class = KampungSerializer
+# class KampungListView(ListAPIView):
+#     queryset = Kampung.objects.all()
+#     serializer_class = KampungSerializer
 
-class KampungDetailView(RetrieveAPIView):
-    queryset = Kampung.objects.all()
-    serializer_class = KampungSerializer
+# class KampungDetailView(RetrieveAPIView):
+#     queryset = Kampung.objects.all()
+#     serializer_class = KampungSerializer
 
 # END API Kampung
 
 # Start API Order
 
-class OrderListView(ListAPIView):
-    queryset = Order
-    serializer_class = OrderItemSerializer
+# class OrderListView(ListAPIView):
+#     queryset = Order
+#     serializer_class = OrderItemSerializer
